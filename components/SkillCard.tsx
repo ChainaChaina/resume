c8fd4b9e2ebdbbf49274e641184b1d7e3364b341
Lucas Borges Moraes <lucasborgesmoraes@gmail.com>
2021-03-18 15:44:25 -0300

    gyroscope

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -4,7 +4,7 @@ import Tilt from 'react-tilt'
 
 export default function SkillCard(props) {
     return (
-        <Tilt className={style.tilt} options={{ max: 60, reset: true, scale: 1.1 }}  >
+        <Tilt className={style.tilt} options={{ max: 60, reset: true, scale: 1.1,  gyroscope: true, gyroscopeMinAngleX:-45, gyroscopeMaxAngleX:45, gyroscopeMinAngleY:-45, gyroscopeMaxAngleY:45 }}  >
             <div className={style.body} >
                 <img src={props.img} alt="" />
                 <h1>{props.skill}</h1>
